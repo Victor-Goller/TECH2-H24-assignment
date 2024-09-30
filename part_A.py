a00ce9e42d66576e81c3f25afb7ee424f5b5c24e
Victor Goller <victor.goller@student.nhh.no>
2024-09-30 12:03:51 +0200

    Did some further changes, it is correct now

--- part_A.py
+++ part_A.py
@@ -1,83 +1,111 @@
-"""
-TECH2 mandatory assignment - Part A
-
-Write the implementation of part A of the exercise below.
-"""
 from math import sqrt
 import numpy as np
 
-def std_loops(x):
+def std_loops(x, start, end):
     """
     Compute standard deviation of x using loops.
 
     Parameters
     ----------
-    x: Sequence of numbers
+    x : Sequence of numbers
+    start : int
+        Start index for the interval.
+    end : int
+        End index for the interval.
 
     Returns
     -------
     sd : float
-        Standard deviation of the list of numbers.
+        Standard deviation of the numbers in the interval.
     """
-    x = [1,2,3,4,5]
-
+    interval = x[start:end]
+    n = 5                      # we know that the lengt of the interval is 5
     sum_i = 0
     squ = 0
-    n = 5
 
-    for i in range(1,6):
+    for i in interval:
         sum_i = sum_i + i
         squ = squ + i**2
     
-    var = squ/n - (sum_i/n)**2
-
+    mean_i = sum_i / n
+    mean_squ = squ / n
+    var = mean_squ - mean_i**2
     sd = sqrt(var)
-print("Using loops:")
-print(f"The deviation is equal to: {sd}\n")
+    
+    return sd
 
 
-def std_builtin(x):
+def std_builtin(x, start, end):
     """
-    Compute standard deviation of x using the built-in functions sum()
-    and len().
+    Compute standard deviation of x using built-in functions sum() and len().
 
     Parameters
     ----------
-    num_lst: Sequence of numbers
+    x : Sequence of numbers
+    start : int
+        Start index for the interval.
+    end : int
+        End index for the interval.
 
     Returns
     -------
-    deviation : float
-        Standard deviation of the list of numbers.
+    sd : float
+        Standard deviation of the numbers in the interval.
     """
-    num_lst_squ = [1**2, 2**2, 3**2, 4**2, 5**2]
+    interval = x[start:end]
+    n = len(interval)
+    sum_i = sum(interval)
+    squ = sum([i**2 for i in interval])
 
-    mean_ = sum(num_lst)/len(num_lst)
-    squ_ = sum(num_lst_squ)/len(num_lst_squ)
-    variance = squ_ - mean_**2
-    deviation = sqrt(variance)
-print("Using built-in functions sum() and len():")
-print(f"The deviation is equal to: {deviation}\n")
-
-
-
-"""
-Compute standard deviation of num_lst using NumPy.
+    mean_i = sum_i / n
+    mean_squ = squ / n
+    var = mean_squ - mean_i**2
+    sd = sqrt(var)
     
-Parameters
-----------
-num_lst: Sequence of numbers
-Returns
+    return sd
 
--------
-deviation_ : float
-    Standard deviation of the list of numbers.
-"""
 
-deviation_ = np.std(num_lst)
-print("Using NumPy:")
-print(f"The deviation is equal to: {deviation_}\n")
+def std_numpy(x, start, end):
+    """
+    Compute standard deviation of x using NumPy.
+
+    Parameters
+    ----------
+    x : Sequence of numbers
+    start : int
+        Start index for the interval.
+    end : int
+        End index for the interval.
+
+    Returns
+    -------
+    sd : float
+        Standard deviation of the numbers in the interval.
+    """
+    interval = np.array(x[start:end])
+    return np.std(interval)
 
 
 if __name__ == '__main__':
-    pass
+    
+    # Our list
+    x = [1, 2, 3, 4, 5]
+
+    # Set the interval
+    start = 0
+    end = len(x)
+
+    # Using loops
+    sd_loops = std_loops(x, start, end)
+    print("Using loops:")
+    print(f"The deviation is equal to: {sd_loops}\n")
+
+    # Using built-in functions
+    sd_builtin = std_builtin(x, start, end)
+    print("Using built-in functions sum() and len():")
+    print(f"The deviation is equal to: {sd_builtin}\n")
+
+    # Using NumPy
+    sd_numpy = std_numpy(x, start, end)
+    print("Using NumPy:")
+    print(f"The deviation is equal to: {sd_numpy}\n")
